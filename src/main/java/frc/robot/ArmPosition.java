1570dd4da26df31d6b76771962e1b00eedf0df75
Manzano FRC <manzanofrc@gmail.com>
2022-03-12 10:30:06 -0700

    Refactor some constants

---
+++ src/main/java/frc/robot/ArmPosition.java
@@ -0,0 +1,8 @@
+package frc.robot;
+
+
+/** The possible positions for the arm to be in. */
+public enum ArmPosition {
+  UP,
+  DOWN;
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,6 +1,6 @@
 /*
   2022 everybot code
-  written by carson graf 
+  written by carson graf
   don't email me, @ me on discord
 */
 
@@ -21,9 +21,11 @@ import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import static frc.robot.ArmPosition.UP;
+import static frc.robot.ArmPosition.DOWN;
 
 public class Robot extends TimedRobot {
-  
+
   //
   // Hardware definitions
   //
@@ -40,30 +42,40 @@ public class Robot extends TimedRobot {
   PS4Controller driverController = new PS4Controller(0);
   PS4Controller armController = new PS4Controller(1);
 
-  //Constants for controlling the arm. consider tuning these for your particular robot
-  final double armHoldUp = 0.08;
-  final double armHoldDown = 0.13;
-  final double armTravel = 0.5;
+  // Constants for controlling the arm. consider tuning these for your particular
+  // robot
+  private static final double ARM_HOLD_UP = 0.08;
+  private static final double ARM_HOLD_DOWN = 0.13;
+  private static final double ARM_TRAVEL = 0.5;
 
-  final double armTimeUp = 0.5;
-  final double armTimeDown = 0.35;
+  private static final double ARM_TIME_UP = 0.5;
+  private static final double ARM_TIME_DOWN = 0.35;
 
-  //Varibles needed for the code
-  boolean armUp = true; //Arm initialized to up because that's how it would start a match
+  // Varibles needed for the code
+
+
+  /**
+   * Whether or not the arm is currently lifted in the "up position."
+   *
+   * This is initialized to "up", because that's how it would start a match.
+   */
+  ArmPosition armPosition = ArmPosition.UP;
   boolean burstMode = false;
+
+  /** The time at which the arm was instructed to change direction. */
   double lastBurstTime = 0;
 
   double autoStart = 0;
   boolean goForAuto = false;
 
-
   /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
    */
   @Override
   public void robotInit() {
-    //Configure motors to turn correct direction. You may have to invert some of your motors
+    // Configure motors to turn correct direction. You may have to invert some of
+    // your motors
     driveLeftA.setInverted(true);
     driveLeftA.burnFlash();
     driveLeftB.setInverted(true);
@@ -72,61 +84,58 @@ public class Robot extends TimedRobot {
     driveRightA.burnFlash();
     driveRightB.setInverted(false);
     driveRightB.burnFlash();
-    
+
     arm.setInverted(false);
     arm.setIdleMode(IdleMode.kBrake);
     arm.burnFlash();
 
-    //add a thing on the dashboard to turn off auto if needed
+    // add a thing on the dashboard to turn off auto if needed
     SmartDashboard.putBoolean("Go For Auto", false);
     goForAuto = SmartDashboard.getBoolean("Go For Auto", false);
   }
 
   @Override
   public void autonomousInit() {
-    //get a time for auton start to do events based on time later
+    // get a time for auton start to do events based on time later
     autoStart = Timer.getFPGATimestamp();
-    //check dashboard icon to ensure good to do auto
+    // check dashboard icon to ensure good to do auto
     goForAuto = SmartDashboard.getBoolean("Go For Auto", false);
   }
 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    //arm control code. same as in teleop
-    if(armUp){
-      if(Timer.getFPGATimestamp() - lastBurstTime < armTimeUp){
-        arm.set(armTravel);
+    // arm control code. same as in teleop
+    if (this.armPosition == ArmPosition.UP) {
+      if (Timer.getFPGATimestamp() - lastBurstTime < ARM_TIME_UP) {
+        arm.set(ARM_TRAVEL);
+      } else {
+        arm.set(ARM_HOLD_UP);
       }
-      else{
-        arm.set(armHoldUp);
-      }
-    }
-    else{
-      if(Timer.getFPGATimestamp() - lastBurstTime < armTimeDown){
-        arm.set(-armTravel);
-      }
-      else{
-        arm.set(-armHoldUp);
+    } else {
+      if (Timer.getFPGATimestamp() - lastBurstTime < ARM_TIME_DOWN) {
+        arm.set(-ARM_TRAVEL);
+      } else {
+        arm.set(-ARM_HOLD_DOWN);
       }
     }
-    
-    //get time since start of auto
+
+    // get time since start of auto
     double autoTimeElapsed = Timer.getFPGATimestamp() - autoStart;
-    if(goForAuto){
-      //series of timed events making up the flow of auto
-      if(autoTimeElapsed < 3){
-        //spit out the ball for three seconds
+    if (goForAuto) {
+      // series of timed events making up the flow of auto
+      if (autoTimeElapsed < 3) {
+        // spit out the ball for three seconds
         intake.set(ControlMode.PercentOutput, -1);
-      }else if(autoTimeElapsed < 6){
-        //stop spitting out the ball and drive backwards *slowly* for three seconds
+      } else if (autoTimeElapsed < 6) {
+        // stop spitting out the ball and drive backwards *slowly* for three seconds
         intake.set(ControlMode.PercentOutput, 0);
         driveLeftA.set(-0.3);
         driveLeftB.set(-0.3);
         driveRightA.set(-0.3);
         driveRightB.set(-0.3);
-      }else{
-        //do nothing for the rest of auto
+      } else {
+        // do nothing for the rest of auto
         intake.set(ControlMode.PercentOutput, 0);
         driveLeftA.set(0);
         driveLeftB.set(0);
@@ -138,15 +147,16 @@ public class Robot extends TimedRobot {
 
   /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+  }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    //Set up arcade steer
-    double leftDrive = -driverController.getLeftY();//.getRawAxis(1);
-    double rightDrive = -driverController.getRightY();//.getRawAxis(2);
-    
+    // Set up arcade steer
+    double leftDrive = -driverController.getLeftY();// .getRawAxis(1);
+    double rightDrive = -driverController.getRightY();// .getRawAxis(2);
+
     double driveLeftPower = leftDrive;
     double driveRightPower = rightDrive;
 
@@ -155,50 +165,45 @@ public class Robot extends TimedRobot {
     driveRightA.set(driveRightPower);
     driveRightB.set(driveRightPower);
 
-    //Intake controls
-    if(armController.getRawButton(0)){
-      intake.set(VictorSPXControlMode.PercentOutput, 1);;
-    }
-    else if(armController.getRawButton(2)){
+    // Intake controls
+    if (armController.getRawButton(0)) {
+      intake.set(VictorSPXControlMode.PercentOutput, 1);
+    } else if (armController.getRawButton(2)) {
       intake.set(VictorSPXControlMode.PercentOutput, -1);
-    }
-    else{
+    } else {
       intake.set(VictorSPXControlMode.PercentOutput, 0);
     }
 
-    //Arm Controls
-    if(armUp){
-      if(Timer.getFPGATimestamp() - lastBurstTime < armTimeUp){
-        arm.set(armTravel);
+    // Arm Controls
+    if (this.armPosition == UP) {
+      if (Timer.getFPGATimestamp() - lastBurstTime < ARM_TIME_UP) {
+        arm.set(ARM_TRAVEL);
+      } else {
+        arm.set(ARM_HOLD_UP);
       }
-      else{
-        arm.set(armHoldUp);
-      }
-    }
-    else{
-      if(Timer.getFPGATimestamp() - lastBurstTime < armTimeDown){
-        arm.set(-armTravel);
-      }
-      else{
-        arm.set(-armHoldDown);
+    } else {
+      if (Timer.getFPGATimestamp() - lastBurstTime < ARM_TIME_DOWN) {
+        arm.set(-ARM_TRAVEL);
+      } else {
+        arm.set(-ARM_HOLD_DOWN);
       }
     }
-  
-    if(armController.getRawButtonPressed(3) && !armUp){
+
+    if (armController.getRawButtonPressed(3) && this.armPosition == DOWN) {
       lastBurstTime = Timer.getFPGATimestamp();
-      armUp = true;
-    }
-    else if(armController.getRawButtonPressed(1) && armUp){
+      this.armPosition = UP;
+    } else if (armController.getRawButtonPressed(1) && this.armPosition == UP) {
       lastBurstTime = Timer.getFPGATimestamp();
-      armUp = false;
-    }  
+      this.armPosition = DOWN;
+    }
 
   }
 
   @Override
   public void disabledInit() {
-    //On disable turn off everything
-    //done to solve issue with motors "remembering" previous setpoints after reenable
+    // On disable turn off everything.
+    // done to solve issue with motors "remembering" previous setpoints after
+    // reenable
     driveLeftA.set(0);
     driveLeftB.set(0);
     driveRightA.set(0);
@@ -206,5 +211,4 @@ public class Robot extends TimedRobot {
     arm.set(0);
     intake.set(ControlMode.PercentOutput, 0);
   }
-    
 }
